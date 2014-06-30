4f2bbe1359272fbe17f142ac054a2d4e819f8e6f
Alix Fullerton <alix@politeauthority.com>
2014-06-30 16:48:05 -0600

    Adding general configs

---
+++ config/app_config.py
@@ -0,0 +1,12 @@
+#Primary config file, before install this is the only file you must modify
+
+settings = { 
+    'database': {
+        'host'       : "localhost",
+        'user'       : "user",
+        'pass'       : "password",
+        'name'       : "good_consumer",
+    }
+}
+
+# Endfile: config/app_config.py
