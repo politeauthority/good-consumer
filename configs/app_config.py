6760ec4e96a88371a0ef431a927492888cd583ef
Alix <fullertonalix0@gmail.com>
2014-08-13 15:21:31 -0700

    Moving configuration files to a more central location

---
+++ configs/app_config.py
@@ -0,0 +1,23 @@
+"""
+    Primary config file, before install this is the only file you must modify.
+"""
+
+settings = {
+    'server' : {
+        'cdn'            : True,
+        'template_cache' : True,
+        'production'     : False,
+        'debug'          : True
+    },
+    'data_fetch' :{
+        'tor'            : False,
+    },
+    'database': {
+        'host'       : "localhost",
+        'user'       : "user",
+        'pass'       : "pass",
+        'name'       : "good_consumer",
+    }
+}
+
+# Endfile: config/app_config.py
